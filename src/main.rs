3442c822887a003bff534a3ca191d7ec7daa3e2f
bagel897 <ellenagarwal897@gmail.com>
2025-02-13 10:55:28 -0800

    Refactor and add env logger

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,21 @@ use sysinfo::System;
 struct Args {
     input: String,
 }
-fn main() {
+fn get_memory() -> u64 {
+    let s = System::new_all();
+    let current = s.process(sysinfo::get_current_pid().unwrap()).unwrap();
+    current.memory()
+}
+fn parse_files(dir: String) -> (Vec<Box<tsx::Program>>, Vec<String>) {
     rayon::ThreadPoolBuilder::new()
         .stack_size(1024 * 1024 * 1024 * 10)
         .build_global()
         .unwrap();
-    let args = Args::parse();
-    let dir = args.input;
-    let mut errors = Vec::new();
     let (tx, rx) = crossbeam::channel::unbounded();
-    let start = Instant::now();
+    let mut errors = Vec::new();
     let files_to_parse: Vec<Result<path::PathBuf, glob::GlobError>> =
         glob(&format!("{}/**/*.ts*", dir)).unwrap().collect();
-
+    log::info!("Parsing {} files", files_to_parse.len());
     let files: Vec<Box<tsx::Program>> = files_to_parse
         .par_iter()
         .filter_map(|file| {
@@ -39,15 +41,21 @@ fn main() {
             None
         })
         .collect();
-    let end = Instant::now();
-    let duration: std::time::Duration = end.duration_since(start);
     drop(tx);
     for e in rx.iter() {
         errors.push(e);
     }
-    let s = System::new_all();
-    let current = s.process(sysinfo::get_current_pid().unwrap()).unwrap();
-    let memory = current.memory();
+    (files, errors)
+}
+fn main() {
+    env_logger::init();
+    let args = Args::parse();
+    let dir = args.input;
+    let start = Instant::now();
+    let (files, errors) = parse_files(dir);
+    let end = Instant::now();
+    let duration: std::time::Duration = end.duration_since(start);
+    let memory = get_memory();
     log::info!(
         "{} files parsed in {:?}.{} seconds with {} errors. Using {} MB of memory",
         files.len(),
