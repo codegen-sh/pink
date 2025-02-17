ffd4b77639040836661ab868975d8fbed5a58928
bagel897 <ellenagarwal897@gmail.com>
2025-02-17 15:27:36 -0800

    query parsing pt1

--- codegen-sdk-cst/src/main.rs
+++ codegen-sdk-cst/src/main.rs
@@ -3,6 +3,8 @@ use codegen_sdk_cst::query::HasQuery;
 fn main() {
     for language in LANGUAGES.iter() {
         println!("{}", language.name);
-        println!("{:?}", language.queries());
+        println!("{:#?}", language.queries());
+        println!("{:#?}", language.definitions());
+        println!("{:#?}", language.references());
     }
 }
--- codegen-sdk-cst/src/query.rs
+++ codegen-sdk-cst/src/query.rs
@@ -1,17 +1,70 @@
-use std::collections::HashMap;
-
-use codegen_sdk_common::Language;
-
+use crate::ts_query;
 use crate::CSTLanguage;
+use codegen_sdk_common::{CSTNode, HasChildren, Language};
+use derive_more::Debug;
+use std::collections::HashMap;
+fn captures_for_field_definition(
+    node: &ts_query::FieldDefinition,
+) -> impl Iterator<Item = &ts_query::Capture> {
+    let mut captures = Vec::new();
+    for child in node.children() {
+        match child {
+            ts_query::ChildrenFieldDefinition::Definition(definition) => {
+                captures.extend(captures_for_node(definition));
+            }
+            _ => {}
+        }
+    }
+    captures.into_iter()
+}
+fn captures_for_named_node(node: &ts_query::NamedNode) -> impl Iterator<Item = &ts_query::Capture> {
+    let mut captures = Vec::new();
+    for child in node.children() {
+        match child {
+            ts_query::ChildrenNamedNode::Capture(capture) => captures.push(capture),
+            ts_query::ChildrenNamedNode::Definition(definition) => {
+                captures.extend(captures_for_node(definition));
+            }
+            _ => {}
+        }
+    }
+    captures.into_iter()
+}
+fn captures_for_node(node: &ts_query::Definition) -> impl Iterator<Item = &ts_query::Capture> {
+    let mut captures = Vec::new();
+    match node {
+        ts_query::Definition::NamedNode(named) => captures.extend(captures_for_named_node(named)),
+        ts_query::Definition::FieldDefinition(field) => {
+            captures.extend(captures_for_field_definition(field))
+        }
+        _ => {}
+    }
+    captures.into_iter()
+}
 #[derive(Debug)]
-pub struct Query {}
+#[debug("{}", self.source())]
+pub struct Query {
+    node: ts_query::NamedNode,
+}
 impl Query {
     pub fn from_queries(source: &str) -> HashMap<String, Self> {
-        let parsed = crate::ts_query::Query::parse(source).unwrap();
-        println!("{:#?}", parsed);
+        let parsed = ts_query::Query::parse(source).unwrap();
+        let mut queries = HashMap::new();
+        for node in parsed.children() {
+            if let ts_query::ChildrenProgram::Definition(definition) = node {
+                match definition {
+                    ts_query::Definition::NamedNode(named) => {
+                        let query = Self::from_named_node(named);
+                        queries.insert(query.name(), query);
+                    }
+                    node => {
+                        println!("Unhandled query: {:#?}", node);
+                    }
+                }
+            }
+        }
 
         // let root_node: Node<'a> = tree.root_node();
-        let mut queries = HashMap::new();
         // for child in root_node.children(&mut root_node.walk()) {
         //     if child.kind() == "grouping" {
         //         continue;
@@ -25,18 +78,27 @@ impl Query {
         // }
         queries
     }
-    // /// Get the kind of the query (the node to be matched)
-    // pub fn kind(&self) -> String {
-    //     let name_node = self.query.child_by_field_name("name").unwrap();
-    //     get_text_from_node(&name_node, &self.source)
-    // }
-    // /// Get the name of the query (IE @reference.class)
-    // pub fn name(&self) -> String {
-    //     for node in self.query.named_children(&mut self.query.walk()) {
-    //         if node.kind() == "capture" {
-    //             return get_text_from_node(&node, &self.source);
-    //         }
-    //     }
+    fn from_named_node(named: &ts_query::NamedNode) -> Self {
+        Query {
+            node: named.clone(),
+        }
+    }
+    /// Get the kind of the query (the node to be matched)
+    pub fn kind(&self) -> String {
+        if let ts_query::NameNamedNode::Identifier(identifier) = &(*self.node.name) {
+            return identifier.source();
+        }
+        panic!("No kind found for query. {:#?}", self.node);
+    }
+    fn captures(&self) -> Vec<&ts_query::Capture> {
+        captures_for_named_node(&self.node).collect()
+    }
+    /// Get the name of the query (IE @reference.class)
+    pub fn name(&self) -> String {
+        let mut result = self.captures().last().unwrap().source();
+        result.replace_range(0..1, "");
+        result
+    }
 
     //     for node in self.query.named_children(&mut self.query.walk()) {
     //         for node in node.named_children(&mut self.query.walk()) {
@@ -53,13 +115,29 @@ impl Query {
     //     );
     // }
 
-    // fn source(&self) -> String {
-    //     get_text_from_node(&self.query, &self.source)
-    // }
+    fn source(&self) -> String {
+        self.node.source()
+    }
 }
 
 pub trait HasQuery {
     fn queries(&self) -> HashMap<String, Query>;
+    fn queries_with_prefix(&self, prefix: &str) -> HashMap<String, Query> {
+        let mut queries = HashMap::new();
+        for (name, query) in self.queries().into_iter() {
+            if name.starts_with(prefix) {
+                let new_name = name.split(".").last().unwrap();
+                queries.insert(new_name.to_string(), query);
+            }
+        }
+        queries
+    }
+    fn definitions(&self) -> HashMap<String, Query> {
+        self.queries_with_prefix("definition")
+    }
+    fn references(&self) -> HashMap<String, Query> {
+        self.queries_with_prefix("reference")
+    }
 }
 impl HasQuery for Language {
     fn queries(&self) -> HashMap<String, Query> {
