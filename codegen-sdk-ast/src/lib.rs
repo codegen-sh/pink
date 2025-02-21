7333c1a14b9fc09d81bab23145a566bd245b2bbe
bagel897 <ellenagarwal897@gmail.com>
2025-02-21 09:50:38 -0800

    increase recursion limit

--- codegen-sdk-ast/src/lib.rs
+++ codegen-sdk-ast/src/lib.rs
@@ -1,4 +1,4 @@
-#![recursion_limit = "256"]
+#![recursion_limit = "512"]
 use codegen_sdk_common::{File, HasNode};
 pub use codegen_sdk_cst::*;
 pub trait Named {
--- codegen-sdk-cst/src/lib.rs
+++ codegen-sdk-cst/src/lib.rs
@@ -1,4 +1,4 @@
-#![recursion_limit = "256"]
+#![recursion_limit = "512"]
 #![feature(trivial_bounds)]
 use std::{path::PathBuf, sync::Arc};
 
