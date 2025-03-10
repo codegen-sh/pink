653544be9f48bc73441e21eb3cdcda32cf08ede3
bagel897 <ellenagarwal897@gmail.com>
2025-03-10 13:57:38 -0700

    fix stubgen

--- bindings/python/src/bin/stub_gen.rs
+++ bindings/python/src/bin/stub_gen.rs
@@ -1,8 +1,8 @@
 // use pyo3_stub_gen::Result;
 
-// fn main() -> Result<()> {
-//     // `stub_info` is a function defined by `define_stub_info_gatherer!` macro.
-//     let stub = pink_python::stub_info()?;
-//     stub.generate()?;
-//     Ok(())
-// }
+fn main() -> anyhow::Result<()> {
+    // `stub_info` is a function defined by `define_stub_info_gatherer!` macro.
+    // let stub = pink_python::stub_info()?;
+    // stub.generate()?;
+    Ok(())
+}
