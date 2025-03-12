62d1d5860cd8b0c19594acffe30951d81cf08d6d
bagel897 <ellenagarwal897@gmail.com>
2025-03-12 10:44:04 -0700

    Parse tests!

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,9 +1,22 @@
-use std::{hint::black_box, path::PathBuf};
+use std::{
+    array,
+    fmt::{self, Display},
+    path::PathBuf,
+};
+
+use divan::AllocProfiler;
+use rayon::prelude::*;
+#[global_allocator]
+static ALLOC: AllocProfiler = AllocProfiler::system();
 
 use codegen_sdk_analyzer::Codebase;
-use criterion::{Criterion, criterion_group, criterion_main};
-fn clone_repo(url: String, name: String, tmp_dir: &tempfile::TempDir) -> PathBuf {
-    let repo_path = tmp_dir.path().join(name);
+fn thread_counts() -> Vec<usize> {
+    vec![/* available parallelism */ 0, 1, 4, 8]
+}
+const TMP_PATH: &str = "/tmp/pink-bench";
+
+fn clone_repo(url: String, name: String) -> PathBuf {
+    let repo_path = PathBuf::from(TMP_PATH).join(name);
     if !repo_path.exists() {
         log::info!("Cloning repo: {} to {}", url, repo_path.display());
         let mut fetch_opts = git2::FetchOptions::new();
@@ -15,22 +28,44 @@ fn clone_repo(url: String, name: String, tmp_dir: &tempfile::TempDir) -> PathBuf
     }
     repo_path
 }
-fn parse_nest(path: &PathBuf) {
-    let _ = Codebase::new(path.clone());
+struct Repo {
+    pub name: &'static str,
+    pub url: &'static str,
+}
+impl Display for Repo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+const REPOS: &[Repo] = &[
+    Repo {
+        name: "nest",
+        url: "https://github.com/nestjs/nest",
+    },
+    Repo {
+        name: "react",
+        url: "https://github.com/facebook/react",
+    },
+    Repo {
+        name: "tensorflow",
+        url: "https://github.com/tensorflow/tensorflow",
+    },
+];
+const fn repo_indices() -> [usize; REPOS.len()] {
+    [0, 1, 2]
+}
+#[divan::bench(consts = repo_indices())]
+fn parse<const REPO: usize>(bencher: divan::Bencher) {
+    let repo = &REPOS[REPO];
+    let repo_path = clone_repo(repo.url.to_string(), repo.name.to_string());
+    bencher.bench(|| Codebase::new(repo_path.clone()));
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn main() {
     env_logger::init();
-    let temp_dir = tempfile::tempdir().unwrap();
-    let repo_path = clone_repo(
-        "https://github.com/nestjs/nest".to_string(),
-        "nest".to_string(),
-        &temp_dir,
-    );
-    c.bench_function("parse_nest", |b| {
-        b.iter(|| parse_nest(black_box(&repo_path)))
+    REPOS.par_iter().for_each(|repo| {
+        clone_repo(repo.url.to_string(), repo.name.to_string());
     });
+    // Run registered benchmarks.
+    divan::main();
 }
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
